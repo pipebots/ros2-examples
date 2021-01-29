0e68f289612a577a4622733481676f9fa2f1a792
Andy Blight <a.j.blight@leeds.ac.uk>
2021-01-29 14:09:03 +0000

    Fixed errors found by ament_copyright

--- example_launch/launch/action.launch.py
+++ example_launch/launch/action.launch.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python3
 
+# Copyright 2021 University of Leeds
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     # NOTE: the namespace must match if the two nodes are going to talk to
     # each other!
     # NOTE: the name value overrides the name of the node specified in the
     # code.
-    return LaunchDescription([
-        Node(
-            package='action_server',
-            namespace='action_test_ns',
-            executable='action_server_exec',
-            name='s_s'
-        ),
-        Node(
-            package='action_client',
-            namespace='action_test_ns',
-            executable='action_client_exec',
-            name='s_c'
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="action_server",
+                namespace="action_test_ns",
+                executable="action_server_exec",
+                name="s_s",
+            ),
+            Node(
+                package="action_client",
+                namespace="action_test_ns",
+                executable="action_client_exec",
+                name="s_c",
+            ),
+        ]
+    )
